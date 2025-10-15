33fb16b634f20071ff23d12255f2dcae248c97ac
Anon <fuadikamil45@gmail.com>
2025-10-15 20:54:24 +0700

    db

--- db/database.sql
+++ db/database.sql
@@ -1,22 +1,21 @@
-CREATE TABLE status (
-  id_status INT AUTO_INCREMENT PRIMARY KEY,
-  nama_status VARCHAR(100) NOT NULL
+CREATE TABLE mata_kuliah (
+  id_matkul INT AUTO_INCREMENT PRIMARY KEY,
+  nama_matkul VARCHAR(100) NOT NULL,
+  nama_dosen VARCHAR(100)
 );
 
-CREATE TABLE mata_kuliah (
-  id_mk INT AUTO_INCREMENT PRIMARY KEY,
-  nama_mk VARCHAR(150) NOT NULL,
-  dosen_pengampu VARCHAR(100) NOT NULL,
-  sks INT NOT NULL
+CREATE TABLE status (
+  id_status INT AUTO_INCREMENT PRIMARY KEY,
+  nama_status VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE tugas (
   id_tugas INT AUTO_INCREMENT PRIMARY KEY,
-  nama_tugas VARCHAR(150) NOT NULL,
+  nama_tugas VARCHAR(100) NOT NULL,
   deskripsi TEXT,
   deadline DATE,
-  id_mk INT,
+  id_matkul INT,
   id_status INT,
-  FOREIGN KEY (id_mk) REFERENCES mata_kuliah(id_mk) ON DELETE SET NULL,
-  FOREIGN KEY (id_status) REFERENCES status(id_status) ON DELETE SET NULL
+  FOREIGN KEY (id_matkul) REFERENCES mata_kuliah(id_matkul) ON DELETE CASCADE,
+  FOREIGN KEY (id_status) REFERENCES status(id_status) ON DELETE CASCADE
 );
